87f67ec7bb1ca505b7c1986dd184b92001e2db15
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-30 19:40:27 -0300

    refactor: exporting function directly

--- app/connectAPI.js
+++ app/connectAPI.js
@@ -1,10 +1,6 @@
-async function listVideos() {
+export async function listVideos() {
     const conection = await fetch("http://localhost:3000/videos");
     const convert = await conection.json();
-    
-    return convert;
-};
 
-export const connectAPI = {
-    listVideos
+    return convert;
 };
