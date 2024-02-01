2e10ca3f41fcdd62e1a9d9f0dcd970f7028aaad2
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-01 18:44:31 -0300

    refactor: function name

--- app/showVideos.js
+++ app/showVideos.js
@@ -1,8 +1,8 @@
-import { listVideos } from "./connectAPI.js";
+import { listVideosEx } from "./connectAPI.js";
 
 const list = document.querySelector('.videos__container');
 
-function buildCard(titulo, descricao, url, imagem) {
+export default function buildCard(titulo, descricao, url, imagem) {
     const video = document.createElement('li');
     video.className = "videos__item";
     video.innerHTML = `
@@ -31,7 +31,7 @@ function buildCard(titulo, descricao, url, imagem) {
 };
 
 async function listVideo() {
-    const listAPI = await listVideos();
+    const listAPI = await listVideosEx();
     listAPI.forEach(element => list.appendChild(
         buildCard(element.titulo, element.descricao, element.url, element.imagem)));
 };
