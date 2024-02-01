aee4c765708982e25605aa6cad2ab21723a9f452
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-31 22:15:14 -0300

    feat: create video structure param

--- app/connectAPI.js
+++ app/connectAPI.js
@@ -4,3 +4,22 @@ export async function listVideos() {
 
     return convert;
 };
+
+export async function createVideo(titulo, descricao, url, imagem) {
+    const connection = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const convertedConnection = await connection.json();
+    return convertedConnection;
+
+};
